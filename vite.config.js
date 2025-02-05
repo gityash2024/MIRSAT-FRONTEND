9cddc65ff4ca3389c9bc4a95d21d37ddd7b07dce
yash <yash@ith.tech>
2025-02-05 22:01:59 +0530

    merge code

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ export default {
   build: {
     target: 'es2015',
     rollupOptions: {
+      external: [
+        '@fullcalendar/core',
+        '@fullcalendar/react'
+      ],
       output: {
         manualChunks: undefined
       }
@@ -9,6 +13,9 @@ export default {
     commonjsOptions: {
       transformMixedEsModules: true
     },
-    minify: false // Temporarily disable minification
+    minify: false
+  },
+  optimizeDeps: {
+    include: ['@fullcalendar/core', '@fullcalendar/react']
   }
 }
