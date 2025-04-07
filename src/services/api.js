9ec48bd017547629ec560bbd85f7fc6b63d10505
Yash jangid <yash@ith.tech>
2025-04-07 16:29:39 +0530

    merge code

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,7 @@ let backoffRetryCount = 0;
 const MAX_RETRIES = 3;
 
 const api = axios.create({
-  baseURL:'http://localhost:5000/api/v1',
+  baseURL:'https://mirsat-backend.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
