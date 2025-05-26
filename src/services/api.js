0d2a19fa79ff5bdbc9f567b9d513675de3f429e9
yash jangid <yashjangid@yashs-MacBook-Air.local>
2025-05-26 19:33:48 +0530

    url update

--- src/services/api.js
+++ src/services/api.js
@@ -11,8 +11,8 @@ let backoffRetryCount = 0;
 const MAX_RETRIES = 3;
 
 const api = axios.create({
-  // baseURL:'https://mirsat-backend.onrender.com/api/v1',
-  baseURL:'http://localhost:5001/api/v1',
+  baseURL:'https://mirsat-backend.onrender.com/api/v1',
+  // baseURL:'http://localhost:5001/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
